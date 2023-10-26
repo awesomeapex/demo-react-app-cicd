8f781fe1445c797cf7e729f08a71e894a012457b
awesomeapex <149040541+awesomeapex@users.noreply.github.com>
2023-10-26 13:14:39 +0530

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         This is Aryan making changes
+         hello world
         </p>
         <a
           className="App-link"
